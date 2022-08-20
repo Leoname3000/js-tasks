66fbea43a4dd6d62cb12ab148001764a45c9ad90
Leonid Sulin <sulin.leonid@gmail.com>
2022-08-20 22:39:36 +0300

    add task 3 add task 3

---
+++ task3.js
@@ -0,0 +1,53 @@
+const readlineSync = require('readline-sync');
+
+let numberLength = readlineSync.question('Сколько чисел загадывать? (от 3 до 6): ');
+
+let trueNumber = '';
+let trueNumberSet = new Set();
+
+for (let counter = numberLength; counter > 0;) {
+    let digit = String(Math.floor(Math.random() * 10));
+    if (trueNumberSet.has(digit) == false) {
+        trueNumberSet.add(digit);
+        trueNumber += `${digit}`;
+        counter--;
+    }
+}
+
+//console.log(test);
+console.log(numberLength);
+console.log(trueNumber);
+console.log(trueNumberSet);
+
+let tries = readlineSync.question('За сколько попыток отгадаете?: ');
+let isVictory = false;
+
+for (let counter = 0; counter < tries; counter++) {
+
+    let fullMatchDigits = new Array();
+    let matchDigits = new Array();
+
+    let guessNumber = String(readlineSync.question(`(Попытка ${counter + 1}) Введите число: `));
+
+    for (let pos = 0; pos < numberLength; pos++) {
+        if (trueNumber[pos] == guessNumber[pos]) {
+            fullMatchDigits[fullMatchDigits.length] = guessNumber[pos];
+        }
+        else if (trueNumberSet.has(guessNumber[pos]) == true) {
+            matchDigits[matchDigits.length] = guessNumber[pos];
+        }
+    }
+
+    if (fullMatchDigits.length == numberLength) {
+        console.log('Вы угадали число!');
+        isVictory = true;
+        break;
+    }
+    else {
+        console.log(`Совпавших цифр не на своих местах - ${matchDigits.length}: (${matchDigits}), цифр на своих местах - ${fullMatchDigits.length}: (${fullMatchDigits}).`);
+    }
+}
+
+if (isVictory == false) {
+    console.log('Попытки закончились!');
+}
